4e2f4e441b65231f2a1092d636aae04fbee1f198
Jin <thinkjin99@gmail.com>
2023-02-04 21:06:22 +0900

    Add crew login
    crew 로그인 api를 추가했습니다.

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from constant import DATABASE_URL
 
-DATABASE_URL = "mysql+pymysql://root:kidok0714@localhost:3306/ssung"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
