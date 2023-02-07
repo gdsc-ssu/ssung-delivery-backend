005eb1e84d4e839eb6cdcfe05f76a2b8ef46fb23
citrusinesis <jihojiho2003@gmail.com>
2023-02-07 23:17:28 +0900

    fix `models.py`

--- models.py
+++ models.py
@@ -25,15 +25,5 @@ class Crew(Base):
     modified_at = Column(DateTime, default=func.now())
 
 
-class Crew(Base):
-    __tablename__ = 'crews'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    crew_name = Column(String(30), nullable=False, unique=True)
-    password = Column(String(200), nullable=False)
-    area = Column(String(50), nullable=False)
-    phone_number = Column(String(20), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-
-
 # if __name__ == '__main__':
 #     Base.metadata.create_all(engine)
