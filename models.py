7cce0d0b170669102beb5d186f0f7aa51bc984d2
Jin <thinkjin99@gmail.com>
2023-02-11 15:52:01 +0900

    Add shippment orm
    주문을 위한 orm을 추가했고 몇몇 잘못된 타입과 오타를 수정했습니다.

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, MetaData
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, BIGINT, Enum, ForeignKey,MetaData
 from sqlalchemy.sql.expression import func
-from database import *
+import enum
+from database import Base, engine
+
+class Status(enum.Enum):
+    ready = 0
+    ongoing = 1
+    finished = 2
+
 
 
 class User(Base):
@@ -10,8 +17,8 @@ class User(Base):
     password = Column(String(200), nullable=False)
     address = Column(String(50), nullable=False)
     phone_number = Column(String(20), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    modified_at = Column(DateTime, default=func.now())
+    created_at = Column(TIMESTAMP, default=func.now())
+    modified_at = Column(TIMESTAMP, default=func.now())
 
 
 class Crew(Base):
@@ -21,19 +28,26 @@ class Crew(Base):
     password = Column(String(200), nullable=False)
     area = Column(String(50), nullable=False)
     phone_number = Column(String(20), nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    modified_at = Column(DateTime, default=func.now())
+    created_at = Column(TIMESTAMP, default=func.now())
 
 
-class Crew(Base):
-    __tablename__ = 'crews'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    crew_name = Column(String(30), nullable=False, unique=True)
-    password = Column(String(200), nullable=False)
-    area = Column(String(50), nullable=False)
-    phone_number = Column(String(20), nullable=False)
-    created_at = Column(DateTime, default=func.now())
+class Shipment(Base):
+    __tablename__ = 'shippments'
+    id = Column(BIGINT, primary_key=True, autoincrement=True)
+    content_id = Column(BIGINT, ForeignKey("contents.id"), nullable=False)
+    crew_id = Column(Integer, ForeignKey("crews.id"), nullable=False)
+    sender_id = Column(Integer, ForeignKey("senders.id"), nullable=False)
+    stats = Column(Enum(Status), default=Status.ready)
+    location = Column(String(255), nullable=False)
+    shipment_detail = Column(String(255), nullable=True) #배송시 주의사항 등
+    destination = Column(String(255), nullable=False)
+    receiver_name = Column(String(255), nullable=True)
+    receiver_phone_number = Column(String(255), nullable=True)
+    shipment_start_date = Column(TIMESTAMP, default=func.now(), nullable=False)
+    shipment_end_date = Column(TIMESTAMP)
 
 
-# if __name__ == '__main__':
-#     Base.metadata.create_all(engine)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
