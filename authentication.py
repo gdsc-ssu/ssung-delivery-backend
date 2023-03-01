5cb1ca7f2313f4a3075221e66871e2f8b4b69b49
citrusinesis <jihojiho2003@gmail.com>
2023-03-01 18:32:52 +0900

    로직 합치기

--- authentication.py
+++ authentication.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
@@ -7,7 +9,7 @@ from starlette import status
 from database import create_session
 from domain.crew.crew_crud import get_crew
 from domain.sender.sender_crud import get_sender
-from models import Sender
+from models import Sender, Crew
 from utils import get_token_subject
 
 credentials_exception = HTTPException(
@@ -20,10 +22,11 @@ sender_scheme = OAuth2PasswordBearer(tokenUrl="/api/sender/login")
 crew_scheme = OAuth2PasswordBearer(tokenUrl="/api/crew/login")
 
 
-def get_current_sender(
-        token: str = Depends(sender_scheme),
-        session: Session = Depends(create_session),
-) -> Sender:
+def get_current_entity(
+        get_entity: Callable[[Session, str], Sender | Crew | None],
+        token: str,
+        session: Session,
+) -> Sender | Crew:
     try:
         username = get_token_subject(token)
         if username is None:
@@ -32,27 +35,22 @@ def get_current_sender(
     except JWTError:
         raise credentials_exception
 
-    sender = get_sender(session, username)
+    sender = get_entity(session, username)
     if sender is None:
         raise credentials_exception
 
     return sender
 
 
+def get_current_sender(
+        token: str = Depends(sender_scheme),
+        session: Session = Depends(create_session),
+) -> Sender:
+    return get_current_entity(get_sender, token, session)
+
+
 def get_current_crew(
         token: str = Depends(crew_scheme),
         session: Session = Depends(create_session),
-) -> Sender:
-    try:
-        username = get_token_subject(token)
-        if username is None:
-            raise credentials_exception
-
-    except JWTError:
-        raise credentials_exception
-
-    crew = get_crew(session, username)
-    if crew is None:
-        raise credentials_exception
-
-    return crew
+) -> Crew:
+    return get_current_entity(get_crew, token, session)
