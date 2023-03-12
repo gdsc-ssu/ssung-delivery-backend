0ac06b0e51c543fa094c5fedffcff46475beb1f0
정명진/커머스전략팀-MI파트 <mjin1789@gmail.com>
2023-03-12 16:44:43 +0900

    Change orm model
    orm 모델을 최신 erd 와 일치시켰습니다.

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, BIGINT, Enum, ForeignKey, false
+from sqlalchemy import Column, Integer, String, JSON, TIMESTAMP, BIGINT, Enum, ForeignKey
 from sqlalchemy.sql.expression import func
 
 from database import Base
@@ -20,7 +20,7 @@ class Sender(Base):
     address = Column(String(50), nullable=False)
     phone_number = Column(String(20), nullable=False)
     created_at = Column(TIMESTAMP, default=func.now())
-    modified_at = Column(TIMESTAMP, default=func.now())
+    updated_at = Column(TIMESTAMP, default=func.now())
 
 
 class Crew(Base):
@@ -33,31 +33,37 @@ class Crew(Base):
     created_at = Column(TIMESTAMP, default=func.now())
 
 
-class Content(Base):
-    __tablename__ = 'contents'
-    id = Column(BIGINT, primary_key=True, autoincrement=True)
-    name = Column(String(255), nullable=False)
-    type = Column(String(255), nullable=False)
-    fragile = Column(Boolean, default=false)
-    express = Column(Boolean, default=false)
-    created_at = Column(TIMESTAMP, default=func.now())
-    modified_at = Column(TIMESTAMP, default=func.now())
+# class Content(Base):
+#     __tablename__ = 'contents'
+#     id = Column(BIGINT, primary_key=True, autoincrement=True)
+#     name = Column(String(255), nullable=False)
+#     type = Column(String(255), nullable=False)
+#     fragile = Column(Boolean, default=false)
+#     express = Column(Boolean, default=false)
+#     created_at = Column(TIMESTAMP, default=func.now())
+#     updated_at = Column(TIMESTAMP, default=func.now())
 
 
 class Shipment(Base):
     __tablename__ = 'shipments'
     id = Column(BIGINT, primary_key=True, autoincrement=True)
-    content_id = Column(BIGINT, ForeignKey("contents.id"), nullable=False)
+    identifier = Column(JSON, nullable=False)
+    history = Column(JSON, nullable=False)
     crew_id = Column(Integer, ForeignKey("crews.id"), nullable=False)
     sender_id = Column(Integer, ForeignKey("senders.id"), nullable=False)
     status = Column(Enum(Status), default=Status.ready)
-    location = Column(String(255), nullable=False)
     shipment_detail = Column(String(255), nullable=True)  # 배송시 주의사항 등
     destination = Column(String(255), nullable=False)
     receiver_name = Column(String(255), nullable=True)
     receiver_phone_number = Column(String(255), nullable=True)
-    shipment_start_date = Column(TIMESTAMP, default=func.now(), nullable=False)
-    shipment_end_date = Column(TIMESTAMP)
+    created_at = Column(TIMESTAMP, default=func.now())
+    updated_at = Column(TIMESTAMP, default=func.now())
+    # content_id = Column(BIGINT, ForeignKey("contents.id"), nullable=False)
+    # location = Column(String(255), nullable=False)
+
+
+    # shipment_start_date = Column(TIMESTAMP, default=func.now(), nullable=False)
+    # shipment_end_date = Column(TIMESTAMP)
 
 # if __name__ == '__main__':
 # Base.metadata.create_all(engine)
