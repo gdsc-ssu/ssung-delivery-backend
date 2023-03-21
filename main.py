da8b141a25f430190fd34e0055f7e97290d3a77f
citrusinesis <jihojiho2003@gmail.com>
2023-03-21 23:10:30 +0900

    edit cors origins

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1",
     "http://localhost",
+    "http://localhost:5174",
     "http://api.ssung.app",
     "https://api.ssung.app",
 ]
