7598f81563b2d6f4fd08f99c76c70a2ed74f27b1
citrusinesis <jihojiho2003@gmail.com>
2023-03-23 23:40:52 +0900

    remove duplicate check

--- domain/common/identifier.py
+++ domain/common/identifier.py
@@ -8,10 +8,4 @@ from utils import encode_id
 def create_identifier(session: Session):
     words = [random.randint(1, session.query(Word).count()) for _ in range(3)]
     id_list = session.query(Word).filter(Word.id.in_(words)).all()
-    id = encode_id([el.word for el in id_list])
-
-    query = session.query(Shipment).filter_by(identifier=id).first()
-    if query:
-        create_identifier(session)
-
-    return id
+    return encode_id([el.word for el in id_list])
